720fb62257ec67ffd123b6c88805d21fff2eb6f8
shreySaxena <shreyyerhs07@gmail.com>
2024-09-08 22:45:29 +0530

    added data

--- setup2.sh
+++ setup2.sh
@@ -1,8 +1,4 @@
-mkdir app
-cd app
-git init
-git remote add origin https://github.com/shrey-007/MovieRecommendation.git
-git pull origin master
-# write something in application.properties
-sudo docker build -t tommy DockerImages/springboot/
-sudo docker run -d -p 8080:8080 --name spring-boot-app tommy
+sudo systemctl start mongod
+cd DockerImages/springboot
+mongoimport --db movies --collection movies --type csv --file /PythonAPI/movies.csv --headerline
+java -jar MoviesRecommender-0.0.1-SNAPSHOT.jar
