0a96de4c3b3522d3717930e3da2a2113d9436570
shreySaxena <shreyyerhs07@gmail.com>
2024-09-09 12:24:39 +0530

    automated the process of running of application

--- setup2.sh
+++ setup2.sh
@@ -5,6 +5,12 @@ sudo systemctl start mongod
 cd PythonAPI
 mongoimport --db movies --collection movies --type csv --file movies.csv --headerline
 
+cd ..
+
+# run python api, but .pkl files git se jab aa rhi hai toh unme error hai toh Filezilla se main folder app mai app.py,dockerfile,.pkl,requirements.txt daalo instead of fetching from git
+sudo docker build -t flaskapp .
+sudo docker run -d -p 5000:5000 flaskapp
+
 # run application
 cd DockerImages/springboot
 java -jar MoviesRecommender-0.0.1-SNAPSHOT.jar
