8b8e46ef59df7ea9ae2edc92c3bf2d6d307fbcf9
shreySaxena <shreyyerhs07@gmail.com>
2024-09-28 00:42:16 +0530

    Overrided the equals() and hashcode() method, because even after using Set datastructure, dashboard was showing duplicate movies

--- src/main/java/com/MoviesRecommender/moviesModule/entity/Movie.java
+++ src/main/java/com/MoviesRecommender/moviesModule/entity/Movie.java
@@ -3,6 +3,8 @@ package com.MoviesRecommender.moviesModule.entity;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Document("movies")
 @Getter
 @ToString
@@ -14,4 +16,17 @@ public class Movie {
     String title;
 
     String overview;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(title, movie.title); // Compare based on title, since title is unique, I have already done data preprocessing
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title); // Ensure the hashCode is based on title, since title is unique, I have already done data preprocessing
+    }
 }
